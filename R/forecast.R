148f99e6465d854fea7d1f159b35d1021d9b57da
David Champredon <davidchampredon@users.noreply.github.com>
2026-03-05 10:35:05 -0500

    forecast without simulation (in progress)

--- R/forecast.R
+++ R/forecast.R
@@ -5,7 +5,7 @@
 #' (e.g. "hospadm" or "h").
 #' Do not specify the age group, all age groups will be returned.
 #' @param simulate Logical. If \code{FALSE}, will simply use the posterior
-#' simulations of the fit object. This implicitly constrain the time horizon
+#' simulations of the fit object. This implicitly constrains the time horizon
 #' and the number of forecast trajectories.
 #' @param prm.fcst List. Forecast parameters.
 #'
@@ -17,25 +17,103 @@
 #' 
 forecast <- function(
     fitobj, 
-    varnames,
     simulate,
     prm.fcst) {
   
   if(0) {
-    varnames = c('hospadm', 'testpos')
+    
+    prm.fcst = list(
+      varnames = c('hospadm', 'testpos'),
+      asof = as.Date.character('2026-05-15')
+    )
   }
+  
+  varnames = prm.fcst[['varnames']]
   
   # Converts to simulation variable names
   nag = get_nag(fitobj[['obj']]) 
   v = sapply(varnames, get_sim_varnames, nag = nag ) |> as.vector()
-  v
-  if(!simulate){
-    # Thu Feb 19 08:49:41 2026 ------------------------------
-    # STOPPED HERE
-    # IMPORTANT: Before implementing, change the simulate() function
-    # such that "data" and "simulation" variable names are the SAME!
-    # i.e. h_2 ==> hospadm_2, tau_1 ==> testpos_1
-    # (Leave the C code with short name variable)
-  }
+  # v
   
+  d = get_last_date_data(fitobj)
+  date.last.data = max(unlist(d)) |> as.Date.numeric(origin = "1970-01-01") 
+  
+  if(!simulate){
+    tmp = fitobj$simpost
+    # Filter dates after last data
+    traj.fcst = tmp[tmp$date > date.last.data, ]
+  }
+  res = list(
+    asof = prm.fcst[['asof']],
+    traj = traj.fcst,
+    fitobj = fitobj
+  )
 }
+
+
+
+### Application example ####
+
+if(0){
+  
+  devtools::load_all()
+  library(ggplot2)
+  library(dplyr)
+  library(tidyr)
+  
+  model.prms = example_model_prms()
+  date.obs = model.prms$date.start + 12*c(1:10)
+  data = example_simulated_data(model.prms = model.prms, 
+                                date.obs = date.obs)
+  
+  # Parameters for the fitting algorithm
+  prms.fit = list(
+    prms.to.fit   = c('R', 'odds.testpos', 'h.prop'),
+    data.used.fit = c('testpos', 'hospadm'),
+    p.accept      = 5e-3,
+    priors.dist = list(
+      n.priors     = 5e3,
+      R            = c('unif', 0.1, 1.3),
+      odds.testpos = c('unif', 0.9, 10),
+      h.prop       = c('unif', 0.0, 0.08)
+    ),
+    n.cores = 3
+  )
+  
+  # Starting point model to feed fit
+  obj = amrem::create(model.prms)
+
+  system.time({
+    fitobj = fit(obj, 
+                 prms.fit = prms.fit, 
+                 data = data)  
+  })  
+   
+  ci = 0.90
+  g.fit.traj = plot_fit_traj(fitobj = fitobj, ci = ci)
+  g.fit.traj
+  
+  # Forecasts
+  
+  prm.fcst = list(
+    varnames = c('hospadm', 'testpos'),
+    asof = as.Date.character('2026-05-15')
+  )
+  
+  fcstobj = forecast(fitobj, 
+                     prm.fcst = prm.fcst,
+                     simulate = FALSE)
+  
+  # Thu Mar  5 10:32:10 2026 ------------------------------
+  # STOPPED HERE
+  g = fcstobj[['traj']] |> 
+    pivot_longer(cols = -c(time, date,idx)) |> 
+    filter(stringr::str_detect(name, 
+                               stringr::str_c(prm.fcst$varnames, 
+                                              collapse = "|"))) |>
+    ggplot(aes(x=date, y = value, group = idx))+ 
+    facet_wrap(~name, scales = 'free_y') + 
+    geom_line()
+  g  
+}
+
