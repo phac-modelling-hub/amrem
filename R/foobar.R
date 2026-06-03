a7e6b140dd6e9e7020f2b604f3e3c3e22d29b22b
David Champredon <davidchampredon@users.noreply.github.com>
2026-06-03 15:36:35 -0400

    test/debug script

---
+++ R/foobar.R
@@ -0,0 +1,120 @@
+
+###
+###  File for testing and debugging
+###
+
+if(FALSE){
+  
+  library(tidyr)
+  library(dplyr)
+  library(ggplot2) ; theme_set(theme_bw())
+  library(lubridate)
+  library(stringr)
+  library(patchwork)
+  
+  library(amrem)
+  packageVersion('amrem')
+  devtools::load_all()
+
+  set.seed(12345) 
+  
+  
+  # ---- One Age Group ----
+  
+ 
+  data.long = readRDS('ab-rsv.rds') |> 
+    mutate(age_group = 'all')
+  g.data = amrem::plot_data_long(data.long)
+  
+  dictionary = data.frame(
+    amrem = c('testpos', 'hospadm'),
+    name = c('positivity', 'hosp')
+   )
+      
+  data = digest_long_data(data = data.long, dictionary = dictionary)
+  
+  model.prms = example_model_prms_ag(
+    r0 = 1.7,
+    N = 5e6, 
+    S0.prop = 0.75,
+    h.prop = 0.007, 
+    odds.testpos = 20)
+  
+  model.prms$date.start = ymd('2025-07-01')
+  model.prms$horizon = 360
+  
+  # Starting point model to feed fit
+  obj = amrem::create(model.prms)
+  sim = amrem::simulate(obj)
+  plot_timeseries(sim) / g.data
+
+  prms.fit = list(
+    data.used.fit = c('testpos', 'hospadm'),
+    p.accept      = 5e-3,
+    n.priors      = 1e4,
+    priors.dist = list(
+      R            = list(c('unif', 1.00, 1.99)),
+      odds.testpos = list(c('unif', 5, 100)),
+      h.prop       = list(c('beta', 1 , 50))
+    ),
+    n.cores = 1
+  )
+  
+  fitobj = fit(obj, 
+               prms.fit = prms.fit, 
+               data = data)  
+  
+  ci = 0.90
+  g.fit.traj = plot_fit_traj(fitobj = fitobj, ci = ci)
+  # g.fit.traj
+  
+  gfp = plot_fit_post(fitobj = fitobj, ci = ci)
+  
+  g.fit.post = patchwork::wrap_plots(gfp, nrow = 2)
+  
+  tmp2d = plot_fit_post_2d(fitobj)
+  g.fit.2d = patchwork::wrap_plots(tmp2d)
+  
+  gerr = plot_fit_errors(fitobj)
+  g.fit.err = patchwork::wrap_plots(gerr)
+  
+  
+  fname = paste0('amrem-plot-fit-',reem::timestamp_short(), '.pdf')
+  
+  message('saving plot...')
+  pdf(fname, width=24, height = 15)
+  plot(g.fit.traj)
+  plot(g.fit.post)
+  plot(g.fit.2d)
+  plot(g.fit.err)
+  dev.off()
+  
+  # Synthetic data 
+  
+  # date.vec = seq.Date(ymd('2025-09-01'), 
+  #                     ymd('2026-04-01'), 
+  #                     by = '1 week')
+  # n = length(date.vec)
+  # 
+  # pos = data.frame(
+  #   date = date.vec,
+  #   value = rnorm(n=n, mean = 0.001 *exp(0.2 * c(1:n)), sd = 0.03)
+  # )
+  # pos$value[pos$value <0 ] = 0
+  # plot(pos$value)
+  # 
+  # hosp = data.frame(
+  #   date = date.vec + 2,
+  #   value = rpois(n=n, lambda = 1 + pos$value * 1e3) 
+  # )
+  # hosp
+  # 
+  # data = list(
+  #   testpos_1 = pos,
+  #   hospadm_1 = hosp
+  # ) |> flatten_data()
+  
+  
+  
+  
+}
